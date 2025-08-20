49cf30f55b26866a3d82e95056c7f690d22969f0
Denis Moiseev <isdenmois@gmail.com>
2025-08-20 23:46:07 +0300

    fish

---
+++ .config/fish/conf.d/abbrs.fish
@@ -0,0 +1,36 @@
+# git
+abbr --add g git
+abbr --add gti git
+abbr --add gs git status
+abbr --add gco git checkout
+abbr --add gcm git checkout master
+
+# apt
+abbr --add a paru
+abbr --add ai paru -S
+#abbr --add au 'sudo apt update && sudo apt upgrade -V'
+abbr --add au paru -Syu
+abbr --add ar paru -R
+abbr --add as paru
+
+# npm
+# abbr --add nr npm run
+
+# Modern coreutils
+abbr --add cat bat -p
+abbr --add find fd
+abbr --add grep rg
+abbr --add du gdu
+abbr --add df duf
+# abbr --add 
+
+# nvim
+abbr --add vi nvim
+abbr --add nv nvim
+abbr --add v nvim
+
+# sudo
+abbr --add reboot sudo reboot
+
+# utils
+abbr --add tstat sudo turbostat -i 2 -M -s APIC,Avg_MHz,Bzy_MHz,PkgWatt
---
+++ .config/fish/conf.d/ni.fish
@@ -0,0 +1,2 @@
+complete -f nr -a "(ni-completions -s)"
+complete -f nun -a "(ni-completions -d)"
